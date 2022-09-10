f1478e063a4cbf4a50f29f220e22660809710084
Cside <cside.story@gmail.com>
2022-09-10 19:02:30 +0900

    goodbye jquery

--- main.js
+++ main.js
@@ -1,25 +1,19 @@
-$(function () {
-    if (navigator.userAgent.indexOf('Mac OS X')) {
-      $('body').append('<style type="text/css">html, body { font-family: \x27Helvetica\x27, \x27Arial\x27, \x27繝偵Λ繧ｮ繝手ｧ偵ざ Pro W3\x27, \x27Hiragino Kaku Gothic Pro\x27, sans-serif; } </style>');
-    } else {
-      $('body').append('<style type="text/css">html, body { font-family: \x27Helvetica\x27, \x27Arial\x27, \x27繝｡繧､繝ｪ繧ｪ\x27, \x27Meiryo\x27, \x27MS PGothic\x27, sans-serif; } </style>');
-    }
-    var $window = $(window);
+const adjustSize = (textarea) => {
+  textarea.style.width  = (window.innerWidth / 2 - 21) + 'px';
+  textarea.style.height = (window.innerHeight) + 'px';
+};
 
-    ['#textarea-left', '#textarea-right'].forEach(function(id) {
-        var $textarea = $(id);
-        function adjustSize() {
-            $textarea.width(  $window.innerWidth() / 2 - 21);
-            $textarea.height( $window.innerHeight() );
-        }
-        function save() { localStorage[id] = $textarea.val() }
-        var text = localStorage[id];
-        if (text) {
-            $textarea.val(text);
-        }
-        $window.on('resize', function () { adjustSize() });
-        $textarea.on('keydown', save);
-        $textarea.on('blur',    save); // 念のためこちらでも。なんかkeydownだけだと失敗する時ある…。
-        adjustSize();
-    });
-});
+for (const id of ['#textarea-left', '#textarea-right']) {
+  const textarea = document.querySelector(id);
+
+  textarea.value = localStorage[id] ?? "";
+
+  window.addEventListener('resize', () => {
+    adjustSize(textarea);
+  });
+  textarea.addEventListener('input', () => {
+    localStorage[id] = textarea.value;
+  });
+
+  adjustSize(textarea);
+}
