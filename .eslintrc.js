99cb48bcfe9fdc18f2741f21aa833ecb450bf350
Cside <cside.story@gmail.com>
2022-09-27 21:08:02 +0900

    2 spaces

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true, 
+  },
+  parserOptions: {
+    ecmaVersion: "latest",
+  },
+  rules: {
+    "indent": ["error", 2],
+  },
+};
--- main.js
+++ main.js
@@ -1,41 +1,41 @@
 let prevWidth = 0;
 const minWidth = 640;
 const adjustSize = (textarea) => {
-    const currentWidth = window.innerWidth;
-    let columnNum;
-    if ((prevWidth == 0 || prevWidth >= minWidth) && currentWidth < minWidth) {
-        document.querySelector('#textarea-right').style.display = 'none';
-        columnNum = 1;
+  const currentWidth = window.innerWidth;
+  let columnNum;
+  if ((prevWidth == 0 || prevWidth >= minWidth) && currentWidth < minWidth) {
+    document.querySelector('#textarea-right').style.display = 'none';
+    columnNum = 1;
 
-    } else if (prevWidth < minWidth && currentWidth >= minWidth) {
-        document.querySelector('#textarea-right').style.display = 'block';
-        columnNum = 2;
-    }
-    textarea.style.width  = (currentWidth / columnNum - 21) + 'px';
-    textarea.style.height = (window.innerHeight) + 'px';
+  } else if (prevWidth < minWidth && currentWidth >= minWidth) {
+    document.querySelector('#textarea-right').style.display = 'block';
+    columnNum = 2;
+  }
+  textarea.style.width  = (currentWidth / columnNum - 21) + 'px';
+  textarea.style.height = (window.innerHeight) + 'px';
 
-    prevWidth = currentWidth;
+  prevWidth = currentWidth;
 }
 
 document.addEventListener('keydown', ev => {
-    if (ev.key !== "Tab" || ev.isComposing) { return; }
-    event.preventDefault();
-    document.execCommand('insertText', false, '    ');
+  if (ev.key !== "Tab" || ev.isComposing) { return; }
+  event.preventDefault();
+  document.execCommand('insertText', false, '    ');
 });
 
 for (const id of ['#textarea-left', '#textarea-right']) {
-    const textarea = document.querySelector(id);
+  const textarea = document.querySelector(id);
 
-    textarea.value = localStorage[id] ?? "";
+  textarea.value = localStorage[id] ?? "";
 
-    window.addEventListener('resize', () => {
-        adjustSize(textarea);
-    });
-    textarea.addEventListener('input', () => {
-        localStorage[id] = textarea.value;
-    });
-
+  window.addEventListener('resize', () => {
     adjustSize(textarea);
+  });
+  textarea.addEventListener('input', () => {
+    localStorage[id] = textarea.value;
+  });
+
+  adjustSize(textarea);
 }
 
 function input_tab(event) {
