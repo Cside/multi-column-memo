6a1ffd1f72d657c109ce893adbc70c245c1aff35
Cside <cside.story@gmail.com>
2022-09-27 20:18:37 +0900

    取りあえず動く（計算量は最悪）

--- main.js
+++ main.js
@@ -1,7 +1,20 @@
+let prevWidth = 0;
 const adjustSize = (textarea) => {
-  textarea.style.width  = (window.innerWidth / 2 - 21) + 'px';
-  textarea.style.height = (window.innerHeight) + 'px';
-};
+    const currentWidth = window.innerWidth;
+    let columnNum;
+    if (currentWidth < 640) {
+        console.log('col size: 1');
+        document.querySelector('#textarea-right').style.display = 'none';
+        columnNum = 1;
+    } else {
+        console.log('col size: 2');
+        document.querySelector('#textarea-right').style.display = 'block';
+        columnNum = 2;
+    }
+    // これは毎回計算する必要がある
+    textarea.style.width  = (currentWidth / columnNum - 21) + 'px';
+    textarea.style.height = (window.innerHeight) + 'px';
+}
 
 document.addEventListener('keydown', ev => {
     if (ev.key !== "Tab" || ev.isComposing) { return; }
